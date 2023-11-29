1f4f8ebde1cf692896e0eeddf0a30b605883c6d1
Hao-Ting Li <maniac@csie.io>
2023-11-29 22:39:00 +0800

    chore: +s
    Took 45 seconds

--- algorithms/permutations.py
+++ algorithms/permutations.py
@@ -1,17 +1,17 @@
-def permutation(a, i, n):
+def permutations(a, i, n):
     if i == n:
         print(' '.join(map(str, a)))
         return
 
     for j in range(i, n):
         a[i], a[j] = a[j], a[i]
-        permutation(a, i + 1, n)
+        permutations(a, i + 1, n)
         a[i], a[j] = a[j], a[i]
 
 
 def main():
     n = 3
-    permutation(list(range(1, n + 1)), 0, n)
+    permutations(list(range(1, n + 1)), 0, n)
 
 
 if __name__ == '__main__':
