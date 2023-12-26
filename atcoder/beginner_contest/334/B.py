e976e8b77fb784a466271962f1204ee9998ef642
Hao-Ting Li <maniac@csie.io>
2023-12-27 02:45:41 +0800

    feat: a new method for calculating this question
    Took 38 minutes

--- atcoder/beginner_contest/334/B.py
+++ atcoder/beginner_contest/334/B.py
@@ -1,24 +1,29 @@
+# L <= A+KM <= R
+# 1. A+KM >= L
+#    -> K >= (L-A)/M
+#    -> K = ceil((L-A)/M)
+# 2. A+KM <= R
+#    -> K <= (R-A)/M
+#    -> K = floor((R-A)/M)
+def ceil_div(a, b):
+    """
+    ceil(a/b) for integers
+    """
+    return a // b + (1 if a % b != 0 else 0)
+
+
+def floor_div(a, b):
+    """
+    floor(a/b) for integers
+    """
+    return a // b
+
+
 def main():
     a, m, l, r = map(int, input().split())
-    if l < a:
-        k = (a - l) // m
-        l = a - k * m
-    else:
-        k = (l - a + m - 1) // m
-        l = a + k * m
-
-    if a < r:
-        k = (r - a) // m
-        r = a + k * m
-    else:
-        k = (a - r + m - 1) // m
-        r = a - k * m
-
-    if l > r:
-        ans = 0
-    else:
-        ans = (r - l) // m + 1
-
+    k_l = ceil_div(l - a, m)
+    k_r = floor_div(r - a, m)
+    ans = k_r - k_l + 1
     print(ans)
 
 
