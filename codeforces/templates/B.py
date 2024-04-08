ebc04a3cf7b321dbbb408b4f35509fcab2d66fd2
Hao-Ting Li <maniac@csie.io>
2024-04-09 02:38:27 +0800

    feat: add templates
    Took 2 hours 38 minutes

---
+++ codeforces/templates/B.py
@@ -0,0 +1,15 @@
+def solve():
+    return
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        _ = map(int, input().split())
+        ans = solve()
+        print(ans)
+
+
+if __name__ == '__main__':
+    main()
