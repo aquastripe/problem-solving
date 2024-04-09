f7b953570b493e7ceb40406010423284e9c6a85a
Hao-Ting Li <maniac@csie.io>
2024-04-10 05:55:43 +0800

    fix: use list to prevent from TLE
    Took 1 hour 15 minutes

--- codeforces/1955 Div3/D.py
+++ codeforces/1955 Div3/D.py
@@ -1,31 +1,32 @@
-from collections import defaultdict
+a_count = [0] * (10 ** 6 + 1)
+b_count = [0] * (10 ** 6 + 1)
 
 
 def solve(n, m, k, a, b):
-    a_count = defaultdict(int)
-    b_count = defaultdict(int)
-    b_m_set = set()
     for i in range(m):
-        a_count[a[i]] += 1
         b_count[b[i]] += 1
-        b_m_set.add(b[i])
 
     k_ = 0
-    for b_i in b_m_set:
-        k_ += min(a_count[b_i], b_count[b_i])
+    for i in range(m):
+        a_count[a[i]] += 1
+        k_ += int(b_count[a[i]] - a_count[a[i]] >= 0)
 
     ans = int(k_ >= k)
     for i in range(m, n):
-        a_count[a[i]] += 1
-        if b_count[a[i]] > 0:
-            k_ += int(a_count[a[i]] <= b_count[a[i]])
-
+        k_ -= int(b_count[a[i - m]] - a_count[a[i - m]] >= 0)
         a_count[a[i - m]] -= 1
-        if b_count[a[i - m]] > 0:
-            k_ -= int(a_count[a[i - m]] + 1 <= b_count[a[i - m]])
 
-        if k_ >= k:
-            ans += 1
+        a_count[a[i]] += 1
+        k_ += int(b_count[a[i]] - a_count[a[i]] >= 0)
+
+        ans += k_ >= k
+
+    for i in range(m):
+        a_count[a[i]] = 0
+        b_count[b[i]] = 0
+
+    for i in range(m, n):
+        a_count[a[i]] = 0
 
     return ans
 
